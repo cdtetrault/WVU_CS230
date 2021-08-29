801f1a893bc22b7932c316bd898a7a05ba31f6ff
Cristal <cristaltetrault@loud-20.04>
2021-08-29 04:36:47 -0400

    testing1

---
+++ src/app/header/top-nav-bar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector:'app-top-nav-bar',
+    templateUrl: 'top-nav-bar.component.html'
+})
+
+export class TopNavBarComponent {
+
+}
