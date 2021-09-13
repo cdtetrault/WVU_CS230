a08c4a4e5c6b0c60f207283eeb43f089060a9088
cdtetrault <cdt0020@mix.wvu.edu>
2021-09-12 21:50:53 -0400

    lab3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { TopNavBarComponent } from './header/top-nav-bar.component';
+import { EditNavBarComponent } from './homepage/edit-nav-bar.component';
+import { LayoutMainComponent } from './homepage/layout-main.component';
+import { PostsComponent } from './homepage/posts.component';
+import { SideNavBarComponent } from './homepage/side-nav-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TopNavBarComponent
+    TopNavBarComponent,
+    LayoutMainComponent,
+    PostsComponent,
+    EditNavBarComponent,
+    SideNavBarComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/homepage/side-nav-bar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector:'side-nav-bar',
+    templateUrl: 'side-nav-bar.component.html',
+})
+
+export class SideNavBarComponent {
+
+}
